10317ae183e5bc5ddeb5958413cc970c95023f66
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-08-27 00:56:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-//your JS code here. If required.
 const codes = document.querySelectorAll('.code');
 
 codes.forEach((code, index) => {
     code.addEventListener('input', () => {
         if (code.value.length === 1 && index < codes.length - 1) {
-            codes[index + 1].focused();
+            codes[index + 1].focus();
         }
     });
 
@@ -12,5 +11,9 @@ codes.forEach((code, index) => {
         if (e.key === 'Backspace' && code.value === '' && index > 0) {
             codes[index - 1].focus();
         }
+    });
+    
+    code.addEventListener('focus', (e) => {
+        e.target.select();  // This will select the current value so the user can easily overwrite it.
     });
 });
