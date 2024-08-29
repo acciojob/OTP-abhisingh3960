00f57096eb4288c6bd903071230f856ca9833a1c
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-08-29 22:30:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-const codes = document.querySelectorAll('.code');
-
-codes.forEach((code, index) => {
-    code.addEventListener('input', () => {
-        if (code.value.length === 1 && index < codes.length - 1) {
-            codes[index + 1].focus();
+const codes = document.querySelectorAll(".code");
+codes.forEach((code, idx) => {
+  code.addEventListener("keydown", (event) => {
+    event.preventDefault();
+    if (event.key >= "0" && event.key <= "9") {
+      codes[idx].value = event.key;
+      setTimeout(() => {
+        codes[idx + 1] && codes[idx + 1].focus();
+      }, 10);
+    } else if (event.key === "Backspace") {
+      codes[idx].value = "";
+      setTimeout(() => {
+        if (codes[idx - 1]) {
+          codes[idx - 1].focus();
+          codes[idx - 1].value = "";
         }
-    });
-
-    code.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace' && code.value === '' && index > 0) {
-            codes[index - 1].focus();
-        }
-    });
-    
-    code.addEventListener('focused', (e) => {
-        e.target.select();  // This will select the current value so the user can easily overwrite it.
-    });
+      }, 10);
+    }
+  });
 });
