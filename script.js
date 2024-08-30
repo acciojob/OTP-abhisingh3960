f99efbb7f3188f2f3f7106c493188e82d5c2115d
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-08-30 08:39:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const codes = document.querySelectorAll(".code");
 codes.forEach((code, idx) => {
   code.addEventListener("keydown", (event) => {
-    event.preventDefault();
     if (event.key >= "0" && event.key <= "9") {
+      event.preventDefault();
       codes[idx].value = event.key;
       setTimeout(() => {
         codes[idx + 1] && codes[idx + 1].focus();
       }, 10);
     } else if (event.key === "Backspace") {
+      event.preventDefault();
       codes[idx].value = "";
       setTimeout(() => {
         if (codes[idx - 1]) {
